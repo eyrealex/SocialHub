0fb035886695ac36cad5b30f9ececf9192ee42ce
eyrealex <eyrealex97@gmail.com>
2021-02-14 18:00:17 +0000

    first commit.

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+
+  #set the home#index as the landing page for the website
+  root 'home#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
