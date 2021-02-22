79d16aac826c18a46d1e18a2b9bfca65032a1c4a
eyrealex <eyrealex97@gmail.com>
2021-02-22 20:44:06 +0000

    added posts to home page, create posts button, fixed some ui, added home page back support

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,17 +10,19 @@ class PostsController < ApplicationController
   end
 
   def new
-    @community = params[:community_id]
+    @community = Community.find(params[:community_id])
     @post = Post.new
   end
 
   def create
     @post = Post.new post_values
     @post.account_id = current_account.id
+    @post.community_id = params[:community_id]
 
     if @post.save
       redirect_to community_path(@post.community_id)
     else
+      @community = Community.find(params[:community_id])
       render :new
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
 resources :communities do
-resources :posts
+  resources :posts
 end
 get 'home/index'
 
