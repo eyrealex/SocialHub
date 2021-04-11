0ec679f70ed71d4b907ce2cab14d0656b74c0db4
eyrealex <eyrealex97@gmail.com>
2021-04-11 12:55:24 +0100

    updated test folders

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,14 +1,1 @@
-require 'test_helper'
 
-class PostsControllerTest < ActionDispatch::IntegrationTest
-  test "index action should be success" do
-    get community_post_path(:post_id)
-    assert_response :success
-  end
-
-  test "new should be success" do
-    get new_community_post_path
-    assert_response :success
-  end
-
-end
